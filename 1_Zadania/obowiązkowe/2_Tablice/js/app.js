c5d3680dd2968891f6f01222242aaeef3b97f770
kamilwegrzyn <48628515+kamilwegrzyn@users.noreply.github.com>
2019-10-06 10:50:20 +0200

    Update app.js
    task2

---
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -0,0 +1,27 @@
+//task1
+function distFromAvarage(numberArray){
+    var avg = 0;
+    numberArray.forEach(element => {
+        avg += element;
+    });
+
+    avg = avg / numberArray.length;
+    console.log(avg);
+    var returnArray = [];
+    numberArray.forEach(element => {
+        returnArray.push(element - avg);
+    });
+    console.log(returnArray);
+    return returnArray;
+}
+
+distFromAvarage([1,2,3,4,5,6,7])
+
+//task2
+let fruits = ['apple', 'orange', 'banana', 'peach', 'pear'];
+console.log(fruits[0]);
+console.log(fruits[fruits.length - 1]);
+for(let count = 0; count < fruits.length; count++){
+console.log(fruits[count]);
+
+}
