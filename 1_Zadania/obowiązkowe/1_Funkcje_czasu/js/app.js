f82715b25a98358b53cfeaa21409aeff87ad32e7
kamilwegrzyn <48628515+kamilwegrzyn@users.noreply.github.com>
2019-10-06 10:30:41 +0200

    Update app.js
    task1

---
+++ 1_Zadania/obowiązkowe/1_Funkcje_czasu/js/app.js
@@ -0,0 +1,13 @@
+function counterHello (number) {
+    var count = 0;
+    var intervalId = setInterval(() => {
+    count ++;
+    console.log('Hello', count);
+    if(count ===number) {
+    clearInterval(intervalId);
+    
+    }
+    }, 500);
+    }
+    counterHello(5)
+    
