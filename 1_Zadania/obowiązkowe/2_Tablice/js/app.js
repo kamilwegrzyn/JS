d00e0087736978b7a60fd86e172846da49375585
kamilwegrzyn <48628515+kamilwegrzyn@users.noreply.github.com>
2019-10-20 11:08:21 +0200

    20.10.19 update

--- 1_Zadania/obowiązkowe/2_Tablice/js/app.js
+++ 1_Zadania/obowiązkowe/2_Tablice/js/app.js
@@ -25,3 +25,58 @@ for(let count = 0; count < fruits.length; count++){
 console.log(fruits[count]);
 
 }
+
+//task3
+let array = ['apple','orange','banana'];
+function printTable(array){
+    for(let i=0; i< array.lenght ; i++)
+    console.log(array[i])
+
+}
+
+//task4
+function multiply(array){
+
+    var temp = 1;
+    for(let i = 0; i<array.lenght; i++){
+        temp *=array[i];
+    }
+    console.log(temp);
+}
+
+//task5
+function getEvenAvarage(array){
+    var temp = 0;
+    var counter = 0;
+    array.forEach(element => {
+        if (element %2 == 0)
+        {
+            temp += element;
+            counter++;
+        
+        }
+    });
+    if(temp == 0)
+    {
+console.log(null);
+
+    }else{
+        console.log(temp / counter);
+    }
+}
+    getEvenAvarage([1,2,3,4,5,6,7])// => 4
+    getEvenAvarage([1,1,1,1])// => null
+    getEvenAvarage([2,8,3,7,4])// => 4.666
+
+
+
+//task6
+
+function sortArray(array)
+{
+    console.log(array.sort((a,b ) => a - b));
+}
+sortArray([145,11,3,64,4,6,10]) //=> [3,4,6,10,11,64,145]
+
+
+//task7
